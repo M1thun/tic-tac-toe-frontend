4ef71ace517462241bdb80ad87fbacbec14b06e8
Mithun <mithunchannayanamath@gmail.com>
2022-08-15 20:44:17 +0530

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BoardService } from './board.service';
+import {Stomp} from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,83 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'board-frontend';
+   title = 'TIC-TAC-TOE';
+   board: number[][] = [[0,0,0], [0,0,0], [0,0,0]];
+   boardData:any;
+   size=3;
+   disabled = true;
+   private stompClient = null;
+   markResponse:any="Start the game!!!";
+
+
+   constructor(private boardService: BoardService) {
+//       boardService.messages.subscribe(msg =>{
+//           this.board=msg.board;
+//       });
+   }
+
+    ngOnInit() {
+
+        this.getBoard();
+        this.connect();
+//       setInterval(() => {
+//                       this.connect();
+//                       }, 3000);
+    }
+
+    mark(x,y){
+        console.log(x);
+        console.log(y);
+        this.boardService.markBoard(x,y).subscribe(data => {
+                                                        console.log(data)
+                                                        this.markResponse=data.toString();
+                                                      }) ;
+    }
+
+    setConnected(connected: boolean) {
+        this.disabled = !connected;
+
+        if (connected) {
+          //this.greetings = [];
+        }
+      }
+
+      connect() {
+        const socket = new SockJS('http://localhost:8080/boardReceiver');
+        this.stompClient = Stomp.over(socket);
+
+        const _this = this;
+        this.stompClient.connect({}, function (frame) {
+          _this.setConnected(true);
+          console.log('Connected: ' + frame);
+
+          _this.stompClient.subscribe('/topic/board', function (data) {
+                      console.log(JSON.parse(data.body).board);
+                      _this.boardData=JSON.parse(data.body);
+                      _this.board=JSON.parse(data.body).board.board;
+
+          });
+        });
+      }
+
+      disconnect() {
+        if (this.stompClient != null) {
+          this.stompClient.disconnect();
+        }
+
+        this.setConnected(false);
+        console.log('Disconnected!');
+      }
+
+  getBoard() {
+      this.markResponse="Start the game!!!";
+      this.boardService.getBoard()
+          .subscribe((data) => {
+          console.log(data)
+          this.boardData=data;
+          this.board=this.boardData.board;
+          });
+  }
+
+
 }
---
+++ src/app/board.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient ,HttpParams,HttpHeaders} from '@angular/common/http';
+import { WebsocketService} from './websocket.service';
+import { Observable, Subject } from "rxjs";
+import { map } from 'rxjs/operators';
+
+export interface Message {
+  size: number;
+  gameName: string;
+  board: number[][];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+   URL = 'http://localhost:8080/tictactoe';
+   wsURL = 'ws://topic/board'
+  public messages: Subject<Message>;
+
+  constructor(private http:HttpClient, private wsService: WebsocketService) {
+  }
+
+    getBoard(){
+     console.log('Request is sent!');
+     return this.http.get(this.URL);
+    }
+
+    markBoard(x,y):Observable<string>{
+      console.log('Request is sent!');
+      const myheader = new HttpHeaders().set('Content-Type', 'application/json')
+
+      let queryParams = new HttpParams();
+      queryParams = queryParams.append('xPos',x);
+      queryParams = queryParams.append('yPos',y);
+
+      console.log(queryParams.toString())
+
+      return this.http.post(this.URL+'/mark',{},{params:queryParams, responseType: 'text' });
+
+    }
+
+
+
+}
