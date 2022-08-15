21ab6f40ddcdad33aa42ea1198072d25da2a7e89
Mithun <mithunchannayanamath@gmail.com>
2022-08-15 23:41:21 +0530

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/board-frontend'));
 
-app.get('/*', (req, res) =U+003E
+app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
 );
 
